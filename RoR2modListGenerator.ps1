bdf407c613c622ed75734b867f4f1657e84816ce
demosche <misan3760@gmail.com>
2024-06-02 13:06:03 +0500

    Fix the generator of list mods

---
+++ RoR2modListGenerator.ps1
@@ -0,0 +1,41 @@
+$reader = New-Object System.IO.StreamReader(Get-ChildItem $PSScriptRoot"\ModdedROR2\mods.yml")
+
+$lines = ''
+$lines+="Drop this profile here<br>"
+$lines+="C:\Users\{USERNAME}\AppData\Roaming\r2modmanPlus-local\RiskOfRain2\profiles\`n`n"
+$lines+="List of mods:<br>"
+$lines+="`n>"
+$c=0
+
+if ($reader -ne $null) {
+    while (!$reader.EndOfStream) {
+        $line = $reader.ReadLine()
+	$modname=''
+	$version=' '
+        if ($line.Contains("displayName")) {
+            $modname+=$line -replace ' displayName: ', ''
+            $lines+=$modname
+        }
+	if ($line.Contains("    major: ")) {
+            $version = $version+ $line -replace '   major: ', ''
+            $lines+=$modname+$version
+        }
+	if ($line.Contains("    minor: ")) {
+	    $version = $version+'.'	
+            $version = $version+ $line -replace '    minor: ', ''
+            $lines+=$modname+$version.trim()
+        }
+	if ($line.Contains("    patch: ")) {
+	    $version = $version+'.'	
+            $version = $version+ $line -replace '    patch: ', ''
+	    $lines+=$modname+$version.trim()+"<br>`n"
+	    $c+=1
+        }
+
+    }
+}
+$lines | Out-File -FilePath $PSScriptRoot"\README.md"
+Write-Host "The mod list is updated." "Number of mods is"$c
+Start-Sleep -Seconds 15
+exit
+Write-Host Script End
